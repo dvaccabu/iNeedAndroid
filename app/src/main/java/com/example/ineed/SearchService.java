d02f7ad95b6f699d2a6aa8f6c39b65cbd14ff519
Abhijeet Waghmare <abhijeetwag@gmail.com>
2022-03-13 18:29:36 -0400

    Add

---
+++ app/src/main/java/com/example/ineed/SearchService.java
@@ -0,0 +1,14 @@
+package com.example.ineed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_service);
+    }
+}
